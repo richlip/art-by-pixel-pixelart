c26eebb35cc0b275d6c2e7745ee16a8c1559deb0
richard <richlip@users.noreply.github.com>
2017-12-03 20:32:10 +0100

    1st upload

---
+++ designs.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  // Globale Variablen
+  const $colorPicker = document.getElementById("colorPicker");
+  const $sizePicker = document.getElementById("sizePicker");
+  const $table = document.getElementById("pixel_canvas");
+
+  // listener
+  $sizePicker.addEventListener('submit', function() {
+    event.preventDefault();
+
+    // Grid Daten ziehen
+    let width = document.getElementById("input_width").value;
+    let height = document.getElementById("input_height").value;
+    makeGrid(width, height);
+  })
+
+  // Grid erstellen
+  function makeGrid(width, height) {
+    $table.innerHTML = '';
+    for (let row = 0; row < width; row++) {
+      let newRow = $table.insertRow();
+        for (let cell= 0; cell < height; cell++ ) {
+          let newCell = newRow.insertCell();
+          newCell.onclick = changeColor;
+        }
+    }
+  }
+
+  // Farben wechseln
+  function changeColor() {
+    this.style.background = $colorPicker.value;
+  }
+
+})();
